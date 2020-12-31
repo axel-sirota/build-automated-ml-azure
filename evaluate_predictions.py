0363856b585e1a001f5574edd8a17b57e7b3d76a
Axel Sirota <axel.sirota@jampp.com>
2020-12-31 02:51:31 -0300

    Delimit dataset

---
+++ evaluate_predictions.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+data = pd.read_csv(filepath_or_buffer='energy-pred.csv',
+                   names=['Date', 'TZ', 'City', 'Code', 'Load', 'Predicted'], skiprows=1)
+accuracy = 100*(1-abs(data['Load'] - data['Predicted'])/data['Load'])
+accuracy.dropna(inplace=True)
+print(accuracy.describe())
+
---
+++ get_predictions.py
@@ -0,0 +1,27 @@
+import copy
+import json
+
+import pandas as pd
+import requests
+
+BASE_URL = "http://3b2f6af5-8ca4-449e-a113-b0726fe8e6be.eastus.azurecontainer.io/score"
+BODY = {
+    "data": [
+        {
+            "Column1": None
+        }
+    ]
+}
+
+def update_request(x):
+    new_body = copy.deepcopy(BODY)
+    new_body["data"][0]["Column1"] = x["Date"]
+    result = json.loads(json.loads(requests.post(url=BASE_URL, json=new_body).text))["forecast"][0]
+    return result
+
+
+data = pd.read_csv(filepath_or_buffer='energy-test.csv',
+                   names=['Date', 'TZ', 'City', 'Code', 'Load'])
+predicted = data.apply(lambda x: update_request(x), axis=1)
+data2 = pd.concat([data, predicted], axis=1)
+data2.to_csv(path_or_buf='energy-pred.csv', index=False)
